dec2b4579e570b716bef1fadc14561ec9a562024
Highestop Chen <highestopchen@gmail.com>
2023-04-06 23:54:27 +0800

    add use-state hook using observable-hooks

---
+++ packages/impl-rx/use-state.ts
@@ -0,0 +1,6 @@
+import { useObservableState } from 'observable-hooks';
+import { IState } from './create-state';
+
+export function useState<T, S extends IState<T>>(state: S) {
+	return useObservableState(state.observableState, state.getState());
+}
