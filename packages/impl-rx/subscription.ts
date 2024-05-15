bb639cc52bdd018ea601ca601c24f723ad903502
ChenYn <highestopchen@gmail.com>
2024-05-15 23:56:49 +0800

    remove server state

--- packages/impl-rx/subscription.ts
+++ packages/impl-rx/subscription.ts
@@ -1,13 +1,12 @@
 import { Subject, takeUntil } from 'rxjs';
 import { IState } from './create-state';
-import { IServerState } from './server-state';
 
 export interface IStateSubscription {
     unsubscribe: () => void;
 }
 
 export function subscribeStateChange<T>(
-    state: IState<T> | IServerState<T>,
+    state: IState<T>,
     callback: (state: T) => any
 ): IStateSubscription {
     const closeState$ = new Subject<void>();
--- packages/impl/use-state.ts
+++ packages/impl/use-state.ts
@@ -1,9 +1,8 @@
 import { useRef, useEffect, useState as _useState } from 'react';
 import { IMutableStateController } from './state-controller';
-import { IServerStateController } from './server-state-controller';
 
 export function useState<T>(
-    controller: IMutableStateController<T> | IServerStateController<T>
+    controller: IMutableStateController<T>
 ) {
     const [state, setState] = _useState<T>(controller.getState());
     const unsubscribeCallback = useRef<(() => void) | null>(null);
